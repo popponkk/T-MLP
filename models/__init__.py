f6760fa0b22169855fdd7753ca241567d412f2de
leifukai <92413447+popponkk@users.noreply.github.com>
2026-04-15 00:17:12 +0800

    cgr ver2.0

--- models/__init__.py
+++ models/__init__.py
@@ -23,3 +23,4 @@ from .agr_tmlp_switch_lite import AGRTMLPSwitchLite
 from .agr_tmlp_switch_lite_td import AGRTMLPSwitchLiteTD
 from .agr_tmlp_switch_hardlite import AGRTMLPSwitchHardLite
 from .cgr_tmlp import CGRTMLP
+from .cgr_tmlp_v2 import CGRTMLPV2
